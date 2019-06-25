72aaeff5675b68d230f99fe0e97f4ddb225f1f93
admin <admin@zhoudc>
2019-06-25 13:02:28 +0800

    voterecord save

--- yjk-web/src/main/java/com/ewcms/yjk/re/service/VoteRecordService.java
+++ yjk-web/src/main/java/com/ewcms/yjk/re/service/VoteRecordService.java
@@ -3,22 +3,18 @@ package com.ewcms.yjk.re.service;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ewcms.common.service.BaseService;
-import com.ewcms.common.utils.EmptyUtil;
 import com.ewcms.yjk.re.entity.ReviewMain;
-import com.ewcms.yjk.re.entity.ReviewProcess;
 import com.ewcms.yjk.re.entity.VoteRecord;
 import com.ewcms.yjk.re.entity.VoteTypeEnum;
 import com.ewcms.yjk.re.repository.VoteRecordRepository;
 import com.ewcms.yjk.sb.entity.AuditStatusEnum;
 import com.ewcms.yjk.sb.entity.DrugForm;
 import com.ewcms.yjk.sb.service.DrugFormService;
-import com.google.common.collect.Maps;
 
 @Service
 public class VoteRecordService extends BaseService<VoteRecord, Long> {
@@ -33,37 +29,6 @@ public class VoteRecordService extends BaseService<VoteRecord, Long> {
 		return (VoteRecordRepository) baseRepository;
 	}
 	
-	public VoteRecord findByUserIdAndReviewMainIdAndDrugFormIdAndReviewProcessIdAndSubmittedTrue(Long userId, Long reviewMainId, Long drugFormId, Long reviewProcessId) {
-		return getVoteRecordRepository().findByUserIdAndReviewMainIdAndDrugFormIdAndReviewProcessIdAndSubmittedTrue(userId, reviewMainId, drugFormId, reviewProcessId);
-	}
-	
-	//专家投票评审监控
-	public Map<DrugForm, Map<String, String>> findUserVote(Long voteUserId){
-		//查询评审是否开启，并检验是否关联申报系统
-		ReviewMain reviewMain = reviewMainService.findByEnabledTrue();
-		if (reviewMain == null || reviewMain.getSystemParameter() == null) return Maps.newHashMap();
-		
-		//评审流程是否存在
-		List<ReviewProcess> reviewProcesses = reviewMain.getReviewProcesses();
-		if (EmptyUtil.isCollectionEmpty(reviewProcesses)) return Maps.newHashMap();
-		
-		//获取申报系统编号，并查询本次初审所有通过的药品记录集
-		List<DrugForm> drugForms = drugFormService.findByAuditStatusAndSystemParameterIdOrderByIdAsc(AuditStatusEnum.passed, reviewMain.getSystemParameter().getId());
-		
-		Map<DrugForm, Map<String, String>> maps = Maps.newHashMap();
-		for (DrugForm drugForm : drugForms) {
-			Map<String, String> processMap = Maps.newHashMap();
-			for (ReviewProcess reviewProcess : reviewProcesses) {
-				VoteRecord voteRecord = findByUserIdAndReviewMainIdAndDrugFormIdAndReviewProcessIdAndSubmittedTrue(voteUserId, reviewMain.getId(), drugForm.getId(), reviewProcess.getId());
-				String voteType = "";
-				if (voteRecord != null) voteType = voteRecord.getVoteTypeEnum().getInfo();
-				processMap.put(reviewProcess.getReviewBaseRule().getRuleCnName(), voteType);
-			}
-			maps.put(drugForm, processMap);
-		}
-		return maps;
-	}
-	
 	public Boolean expertSubmitCurrentReview(Long userId, Long reviewProcessId){
 		if(getVoteRecordRepository().countByUserIdAndReviewProcessIdAndSubmittedTrue(userId, reviewProcessId) == 0){
 			return Boolean.FALSE;
@@ -76,24 +41,24 @@ public class VoteRecordService extends BaseService<VoteRecord, Long> {
 	 * 启动专家新增通用名投票
 	 * 
 	 */
-	public String ExpertStartVoteAddCommonName(Long userId){
+	public String expertStartVoteAddCommonName(Long userId,Long currentReviewProcessId){
 		ReviewMain reviewMainEnable = reviewMainService.findByEnabledTrue();
 		if(reviewMainEnable == null)return "评审还未启动！";
-		ReviewProcess reviewProcess = reviewProcessService.findByReviewBaseRuleRuleNameAndReviewMainId("addCommonName", reviewMainEnable.getId());
-		if(reviewProcess == null)return "新增通用名评审流程未找到！";
-		if(getVoteRecordRepository().countByUserIdAndReviewProcessId(userId, reviewProcess.getId()).intValue()  == 0){//初次进入投票，初始化需要投票的申报新曾通用名的药品
+
+		if(getVoteRecordRepository().countByUserIdAndReviewProcessId(userId, currentReviewProcessId).intValue()  == 0){//初次进入投票，初始化需要投票的申报新曾通用名的药品
 			List<DrugForm> validDrugFormList = drugFormService.findByAuditStatusAndSystemParameterIdAndDeclareCategoryAndReviewedFalseOrderByIdAsc(AuditStatusEnum.passed, reviewMainEnable.getSystemParameter().getId(), "新增通用名");
 			for(DrugForm drugForm:validDrugFormList){
 				VoteRecord vo = new VoteRecord();
 				vo.setSubmitted(Boolean.FALSE);
 				vo.setUserId(userId);
 				vo.setReviewMainId(reviewMainEnable.getId()); 
-				vo.setReviewProcessId(reviewProcess.getId());
+				vo.setReviewProcessId(currentReviewProcessId);
 				vo.setDrugForm(drugForm);
 				getVoteRecordRepository().save(vo);
 			}
 		}
-		return reviewProcess.getId().toString();
+		
+		return currentReviewProcessId.toString();
 	}
 	
 	/**
